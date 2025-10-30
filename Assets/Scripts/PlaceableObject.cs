45102579da038efa5f437d45763e20443029435d
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2025-10-29 20:00:51 -0400

    build system

---
+++ Assets/Scripts/PlaceableObject.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+
+public class PlaceableObject : MonoBehaviour
+{
+    public bool Placed { get; private set; }
+    public Vector3Int Size { get; private set; }
+    private Vector3[] Vertices;
+    private void GetColliderVertexPositionsLocal()
+    {
+        BoxCollider b = gameObject.GetComponent<BoxCollider>();
+        Vertices = new Vector3[4];
+        Vertices[0] = b.center + new Vector3(-b.size.x, -b.size.y, -b.size.z) * 0.5f;
+        Vertices[1] = b.center + new Vector3(b.size.x, -b.size.y, -b.size.z) * 0.5f;
+        Vertices[2] = b.center + new Vector3(b.size.x, -b.size.y, b.size.z) * 0.5f;
+        Vertices[3] = b.center + new Vector3(-b.size.x, -b.size.y, b.size.z) * 0.5f;
+    }
+    /* private void CalculateSizeInCells()
+    {
+        Vector3Int[] vertices = new Vector3Int[Vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(Vertices[i]);
+            vertices[i] = BuildingSystem.current.gridLayout.WorldToCell(worldPos);
+        }
+        Size = new Vector3Int(Math.Abs((vertices[0] - vertices[1]).x), Math.Abs((vertices[0] - vertices[3]).y), 1);
+    } */
+    private void CalculateSizeInCells()
+    {
+        Vector3Int[] vertices = new Vector3Int[Vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(Vertices[i]);
+            vertices[i] = BuildingSystem.current.gridLayout.WorldToCell(worldPos);
+        }
+
+        int sizeX = Math.Abs((vertices[0] - vertices[1]).x);
+        int sizeY = Math.Abs((vertices[0] - vertices[3]).y);
+
+        if (sizeX == 0) sizeX = 1;
+        if (sizeY == 0) sizeY = 1;
+
+        Size = new Vector3Int(sizeX, sizeY, 1);
+    }
+    public Vector3 GetStartPosition()
+    {
+        return transform.TransformPoint(Vertices[0]);
+    }
+
+    private void Start()
+    {
+        GetColliderVertexPositionsLocal();
+        CalculateSizeInCells();
+    }
+    public void Rotate()
+    {
+        transform.Rotate(new Vector3(0, 90, 0));
+        Size = new Vector3Int(Size.y, Size.x, 1);
+        Vector3[] vertices = new Vector3[Vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = Vertices[(i + 1) % Vertices.Length];
+        }
+        Vertices = vertices;
+    }
+    public virtual void Place()
+    {
+        ObjectDrag drag = gameObject.GetComponent<ObjectDrag>();
+        Destroy(drag);
+        Placed = true;
+    }
+}
